3bcc8b0c13ed2e46bd83e693347eab13e0ea5439
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-26 17:58:51 +0530

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import "./App.css";
+import { Button } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <Button colorScheme="blue">Button</Button>;
 }
 

b97b6f9398d58dfc661cc8644e9ee457dfbda61e
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-29 20:34:10 +0530

    refactor fetch data methods

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 interface Genre {
   id: number;
@@ -8,34 +6,6 @@ interface Genre {
   slug: string;
 }
 
-interface FetchGenreResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenres = () => {
-  const controller = new AbortController();
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    apiClient
-      .get<FetchGenreResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    // return () => controller.abort();
-  }, []);
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;

c1c49d5ee6eb2d794d3871720257855766de3ccf
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-29 23:33:34 +0530

    fixed mobile device issue

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import GenreList from "./components/GenreList";
 function App() {
   return (
     <Grid
-      templateAreas={`"nav nav" "aside main"`}
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
       templateColumns={{ base: "1fr", lg: "200px 1fr" }}
     >
       <GridItem area="nav">
